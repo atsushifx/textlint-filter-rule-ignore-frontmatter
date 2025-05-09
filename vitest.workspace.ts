59e1e002e6d23204be86dfcf69ca0632ac16221f
Furukawa Atsushi <atsushifx@gmail.com>
2025-05-09 18:02:02 +0900

    chore(configs): config files move to configs

--- vitest.workspace.ts
+++ vitest.workspace.ts
@@ -1,10 +1,10 @@
-import { defineWorkspace } from 'vitest/config'
+import { defineWorkspace } from 'vitest/config';
 
 export default defineWorkspace([
-  "./shared/configs/vitest.config.base.ts",
-  "./packages/@textlint/textlint-filter-rule-ignore-frontmatter/vitest.config.ts",
-  "./packages/@textlint/textlint-plugin-markdown-with-toml/vitest.config.ts",
-  "./packages/@textlint/e2etest/vitest.config.unit.ts",
-  "./packages/@textlint/e2etest/vitest.config.ts",
-  "./packages/@textlint/markdown-to-ast-with-toml/vitest.config.ts"
-])
+  './shared/configs/vitest.config.base.ts',
+  './packages/@textlint/e2etest/vitest.config.ts',
+  './packages/@textlint/e2etest/vitest.config.unit.ts',
+  './packages/@textlint/textlint-plugin-markdown-with-toml/vitest.config.ts',
+  './packages/@textlint/textlint-filter-rule-ignore-frontmatter/vitest.config.ts',
+  './packages/@textlint/markdown-to-ast-with-toml/vitest.config.ts',
+]);
